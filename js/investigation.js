47e8423925e00031320f2e9d6c03501c42ef5f4c
liuxuesong930721 <liuxuesong930721@163.com>
2019-01-04 14:20:25 +0800

    change_html

--- js/investigation.js
+++ js/investigation.js
@@ -15,61 +15,75 @@ $(function(){
             $(li).append(span);
             $("#training_object_ul").append(li);
         }
-        
-      
     }
     training_object_ul();
   //点击问题1中的ul列表信息
     $("#training_object_box").on("click","#training_object_btn",function select_training_object(){
      var check_training_object = document.getElementsByName("check_training_object");
+    //选择问题1后显示选择信息
+     var selected_message_one="";
+     $("#direction_box").html("");
      for(var c = 0;c<check_training_object.length;c++){
         if(check_training_object[c].checked){
-            console.log(check_training_object[c].value)
+            selected_message_one=selected_message_one+check_training_object[c].value+",";
+            for(var i = 0;i<type.length;i++){
+                if(check_training_object[c].value===type[i].name){
+                    
+                    var child_list=type[i].child_list;
+                    for(var j = 0;j<child_list.length;j++){
+                        //叶子节点
+                        if(child_list[j].is_leaf===1){
+                              console.log("叶子节点")
+                              var leaf_div=$("<div class='investigation_direction_name'></div>");
+                              var leaf_radio=$("<input type='radio'/>");
+                              var leaf_span=$("<span>"+child_list[j].name+"</span>");
+                              var leaf_input=$("<input name='check_direction' type='checkbox'/>");
+                              $(leaf_div).append(leaf_radio);
+                              $(leaf_div).append(leaf_span);
+                              $(leaf_div).append(leaf_input);
+                              $("#direction_box").append(leaf_div);
+                           
+                        }
+                         //非叶子节点
+                        if(child_list[j].is_leaf===0){
+                          console.log("非叶子节点")
+                            //   var un_leaf_box=$("<div class='investigation_un_leaf_box'></div>");
+                            //   var un_leaf_title=$("<div class='investigation_un_leaf_title'>"+child_list[j].name+"</div>");
+                              var un_leaf_div=$("<div class='investigation_direction_name'></div>");
+                              var un_leaf_radio=$("<input type='radio'/>");
+                              var un_leaf_span=$("<span>"+child_list[j].name+"</span><span>》</span>");
+                              $(un_leaf_div).append(un_leaf_radio);
+                              $(un_leaf_div).append(un_leaf_span);
+                              $("#direction_box").append(un_leaf_div);
+                              // var un_leaf_span=$("<span></span>");
+                            //   $(un_leaf_box).append(un_leaf_title);
+                            //   $("#direction_box").append(un_leaf_title);
+                            //   var un_leaf_child_list=child_list[j].child_list;
+                            //   for(var m = 0;m<un_leaf_child_list.length;m++){
+                            //       var un_leaf_div=$("<div class='investigation_direction_name'></div>");
+                            //       var un_leaf_input=$("<input name='check_direction' type='checkbox'/>");
+                            //       var un_leaf_span=$("<span>"+un_leaf_child_list[m].name+"</span>");
+                            //       $(un_leaf_div).append(un_leaf_input);
+                            //       $(un_leaf_div).append(un_leaf_span);
+                            //       $("#direction_box").append(un_leaf_div);
+                            //   }
+                         
+                      }
+      
+                    }
+                }
+            }
+            // console.log(check_training_object[c].value)
+
         }
      }
+     $("#selected_message_one").html(selected_message_one.slice(0,selected_message_one.length-1));
+
         //选择问题1后显示选择信息
     //     $("#selected_message_one").html($(this).html());
     //     //清空问题2里面的内容
     //   $("#direction_box").html("");
     //   //循环出问题1对应的child_list
-    //   for(var i = 0;i<type.length;i++){
-    //       if($(this).html()===type[i].name){
-              
-    //           var child_list=type[i].child_list;
-    //           for(var j = 0;j<child_list.length;j++){
-    //               //叶子节点
-    //               if(child_list[j].is_leaf===1){
-    //                     console.log("叶子节点")
-    //                     var leaf_div=$("<div class='investigation_direction_name'></div>");
-    //                     var leaf_input=$("<input name='check_direction' type='checkbox'/>");
-    //                     var leaf_span=$("<span>"+child_list[j].name+"</span>");
-    //                     $(leaf_div).append(leaf_input);
-    //                     $(leaf_div).append(leaf_span);
-    //                     $("#direction_box").append(leaf_div);
-                     
-    //               }
-    //                //非叶子节点
-    //               if(child_list[j].is_leaf===0){
-    //                 console.log("非叶子节点")
-    //                     var un_leaf_box=$("<div class='investigation_un_leaf_box'></div>");
-    //                     var un_leaf_title=$("<div class='investigation_un_leaf_title'>"+child_list[j].name+"</div>");
-    //                     // var un_leaf_span=$("<span></span>");
-    //                     $(un_leaf_box).append(un_leaf_title);
-    //                     $("#direction_box").append(un_leaf_box);
-    //                     var un_leaf_child_list=child_list[j].child_list;
-    //                     for(var m = 0;m<un_leaf_child_list.length;m++){
-    //                         var un_leaf_div=$("<div class='investigation_direction_name'></div>");
-    //                         var un_leaf_input=$("<input name='check_direction' type='checkbox'/>");
-    //                         var un_leaf_span=$("<span>"+un_leaf_child_list[m].name+"</span>");
-    //                         $(un_leaf_div).append(un_leaf_input);
-    //                         $(un_leaf_div).append(un_leaf_span);
-    //                         $(un_leaf_box).append(un_leaf_div);
-    //                     }
-                   
-    //             }
-
-    //           }
-    //       }
-    //   }
+   
     })
 })
