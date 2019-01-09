6bfbfc572cf12e55c0b6eb1344960af975e1d92c
liuxuesong930721 <liuxuesong930721@163.com>
2019-01-09 11:02:48 +0800

    bug

--- js/reset.js
+++ js/reset.js
@@ -1,20 +1,24 @@
 $(function(){
     //重选的函数
-    function reset_fun(parent_id,id,show_id,button_id){
-        $(parent_id).on("click",$(id),function reset_message(){
+    function reset_fun(id,show_id,button_id){
+        $(id).click(function(){
             $(show_id).removeClass("none");
             $(button_id).html("重选")
-
         })
+        // $(parent_id).on("click",$(id),function reset_message(){
+          
+        //     $(show_id).removeClass("none");
+        //     $(button_id).html("重选")
+        // })
     }
      // 重置问题1
-    reset_fun("#select_training_object","#reset_training_object","#training_object_message","#training_object_btn")  
+    reset_fun("#reset_training_object","#training_object_message","#training_object_btn")  
     // 重置问题2
-    reset_fun("#select_direction","#reset_direction","#direction_message")
+    reset_fun("#reset_direction","#direction_message")
     // 重置问题3
-    reset_fun("#select_course","#reset_course","#course_message")
+    reset_fun("#reset_course","#course_message")
     // 重置问题4
-    reset_fun("#select_company_type","#reset_company_type","#company_type_message")
+    reset_fun("#reset_company_type","#company_type_message")
     // 重置问题5
-    reset_fun("#select_duty_type","#reset_duty_type","#duty_type_message")
+    reset_fun("#reset_duty_type","#duty_type_message")
 })
